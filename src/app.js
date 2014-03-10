9508f81ae4021c66ce33ff5aae161196d405b598
piecyk <piecyk@gmail.com>
2014-03-10 21:29:45 +0100

    add init hack way of fetch obj;

--- src/app.js
+++ src/app.js
@@ -1,22 +1,59 @@
 /** @jsx React.DOM */
 
-var flickr = Backbone.Model.extend({
-
-});
-
-var flickrList = Backbone.Collection.extend({
-    model: flickr
-});
-
-
-var CommentBox = React.createClass({
-    render: function() {
-        return (
-                <div className="commentBox">
-                Hello, world! I am a CommentBox. init
-                </div>
-        );
+// 
+var controller = {
+    fetch: function(params, callback){        
+	// add base params
+	params.api_key = 'f7419c7c353a4812a53523af90e255df';
+	params.format = 'json';
+	
+	$.ajax({
+	    url: 'http://api.flickr.com/services/rest/',
+	    type: 'GET',
+	    dataType: 'jsonp',
+	    data: params,
+	    jsonp: 'jsoncallback',
+	    success: function(data){
+		callback(data);		
+	    },	    
+	    error: function() {
+		callback();		
+	    }
+	});
     }
+};
+
+controller.fetch({
+    per_page: 30,
+    method : 'flickr.photos.search',
+    page:1,
+    text:'dom'
+}, function(ret) {
+    React.renderComponent(
+            <showResponse data={ret} />,
+        document.getElementById('container')
+    );
 });
 
-React.renderComponent(<CommentBox />, document.getElementById('container'));
+var showResponse = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>data:</h1>
+        <DataList data={this.props.data} />
+      </div>
+    );
+  }
+});
+
+var DataList = React.createClass({
+  render: function() {
+    var obj = this.props.data;
+    return (
+      <div>
+        {obj}
+      </div>
+    );
+  }
+});
+
